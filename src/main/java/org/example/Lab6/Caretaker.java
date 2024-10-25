6311643c3dd1372039db5c1b98732e31c00f54b2
Ернар <230709@astanait.edu.kz>
2024-10-25 22:08:09 +0500

    Lab6

---
+++ src/main/java/org/example/Lab6/Caretaker.java
@@ -0,0 +1,13 @@
+package org.example.Lab6;
+import java.util.*;
+class Caretaker {
+    private List<Memento> mementoList = new ArrayList<>();
+
+    public void add(Memento state) {
+        mementoList.add(state);
+    }
+
+    public Memento get(int index) {
+        return mementoList.get(index);
+    }
+}
---
+++ src/main/java/org/example/Lab6/Memento.java
@@ -0,0 +1,13 @@
+package org.example.Lab6;
+
+class Memento {
+    private String state;
+
+    public Memento(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+}
